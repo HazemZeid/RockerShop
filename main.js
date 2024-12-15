2f367e6faa606aa84b6e605a8985de3a04223ae5
HazemZeid <hazemahmed202000@gmail.com>
2024-12-15 13:15:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let search = document.querySelector('.search-box');
+let icon =document.querySelector('#search-icon');
+icon.addEventListener('click',function() {
+    search.classList.toggle('active');
+})
+
